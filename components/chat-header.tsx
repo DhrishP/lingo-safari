2b4ba36f77b97cafb988d2c33baed6ec7edbfa6e
rizzabh <pandeyrishabh966@gmail.com>
2023-10-07 08:39:14 +0530

    Added Footer

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const ChatHeader: FC = () => {
   return (
     <div className="w-full flex gap-3 justify-start items-center text-primary-focus">
-      <div className="flex   text-sm">
+      <div className="flex text-sm">
         <div className="flex gap-1.5 items-center">
           <p className="w-2 h-2 rounded-full bg-purple-500" />
           <p className="font-medium"></p>
